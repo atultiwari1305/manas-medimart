6e08bc21a3d5f6b4347391712523edffbea65e82
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2024-04-30 03:17:45 +0530

    new update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Output, EventEmitter} from '@angular/core';
+import { Component, OnInit ,Output, EventEmitter, NgModule} from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
@@ -7,13 +7,14 @@ import { HomeComponent } from './components/home/home.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule ,HttpClient} from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { CatalogComponent } from './components/catalog/catalog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,RouterLink,RouterLinkActive,FooterComponent,
     LoginComponent,RegisterComponent,FormsModule,ReactiveFormsModule,
-    HttpClientModule,CommonModule, HomeComponent
+    HttpClientModule,CommonModule, HomeComponent, CatalogComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
